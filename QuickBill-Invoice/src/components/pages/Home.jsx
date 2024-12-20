82b979277ee3792ddbf521006d5262fce5a2b24b
Lemis Kutswa <lemiskutswa20@gmail.com>
2024-12-20 07:48:21 +0300

    work on services component

--- QuickBill-Invoice/src/components/pages/Home.jsx
+++ QuickBill-Invoice/src/components/pages/Home.jsx
@@ -7,6 +7,13 @@ export default function Home() {
             <h1>Welcome to the QuickBill company website</h1>
             <h2>Create and delete invoices, and check how we're working towards our financial targets</h2>
             <button>Log in &#8594;</button>
+            <h1 className="services-title">Services</h1>
+            <div className="services">
+            <i class="fa fa-laptop" aria-hidden="true"></i>  
+                <div className="service-div">div 1</div>
+                <div className="service-div">div 2</div>
+                <div className="service-div">div 3</div>
+            </div>
         </main>
     )
 }
